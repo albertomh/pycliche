d664b6a8dc8f79bc118df61b441bcc2861de0d09
Alberto Morón Hernández <alberto.moronh@gmail.com>
2024-11-30 09:30:53 +0000

    test: the validator for the project_name question

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -40,3 +40,43 @@ def test_validator_is_empty(
         with pytest.raises(ValueError) as exc_info:
             _copier_copy_with_project_name()
         assert str(exc_info.value).endswith(error_msg)
+
+
+@pytest.mark.parametrize(
+    "project_name, should_raise",
+    [
+        ("ok_project", False),
+        ("MyProjectName", True),
+        ("dash-it", True),
+        ("2whynumber", True),
+    ],
+)
+def test_validator_project_name(
+    project_name: str,
+    should_raise: bool,
+    copier_copy: Callable[[dict], None],
+    copier_input_data: dict,
+):
+    def _copier_copy_with_project_name():
+        copier_copy(
+            {
+                **copier_input_data,
+                "project_name": project_name,
+            }
+        )
+
+    expected_error_msg = (
+        "Validation error for question 'project_name': project_name must "
+        "start with a letter, be lowercase and may contain underscores"
+    )
+
+    if should_raise:
+        with pytest.raises(ValueError) as exc_info:
+            _copier_copy_with_project_name()
+        assert str(exc_info.value) == expected_error_msg
+
+    else:
+        try:
+            _copier_copy_with_project_name()
+        except Exception as e:
+            pytest.fail(f"Unexpected exception raised: {str(e)}")
