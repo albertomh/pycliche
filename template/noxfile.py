f8d760dfcec9e5cc131fad8645297ef5fd730fbc
Alberto Morón Hernández <albertomh@users.noreply.github.com>
2025-09-23 21:51:55 +0100

    feat(tests): run gen. project's tests in parallel, serial if pdb (#141)

--- template/noxfile.py
+++ template/noxfile.py
@@ -9,8 +9,6 @@ OLDEST_PY, *MIDDLE_PY, LATEST_PY = py_versions
 nox.options.default_venv_backend = "uv"
 nox.options.sessions = [f"tests_with_coverage-{LATEST_PY}"]
 
-run_tests_args = ["pytest", "tests/", "-s", "-vvv", "-W", "always", "--pdb"]
-
 
 def _install_deps(session: nox.Session) -> None:
     session.run_install(
@@ -24,11 +22,26 @@ def _install_deps(session: nox.Session) -> None:
     session.install("-e", ".")
 
 
+def make_pytest_args(posargs: list) -> list[str]:
+    pytest_args = [
+        "--capture=no",
+        "--verbosity=3",
+        "--pythonwarnings=always",
+    ]
+
+    use_pdb = "--pdb" in posargs
+    if not use_pdb:
+        pytest_args.append("--numprocesses=auto")
+
+    return pytest_args
+
+
 @nox.session(python=MIDDLE_PY)
 def tests(session: nox.Session) -> None:
     _install_deps(session)
 
-    session.run(*run_tests_args)
+    pytest_args = make_pytest_args(list(session.posargs))
+    session.run("pytest", "tests/", *pytest_args, *session.posargs)
 
 
 @nox.session(python=[OLDEST_PY, LATEST_PY])
@@ -36,12 +49,15 @@ def tests_with_coverage(session: nox.Session) -> None:
     _install_deps(session)
 
     session.run("coverage", "erase")
+    pytest_args = make_pytest_args(list(session.posargs))
     session.run(
         "coverage",
         "run",
         # "--source", ".",
         "-m",
-        *run_tests_args,
+        "pytest",
+        "tests/",
+        *pytest_args,
         *session.posargs,
     )
     session.run("coverage", "report")
