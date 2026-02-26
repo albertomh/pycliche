0e7099ab8b145ceb6b73464253b150a57e4cd410
Alberto Morón Hernández <albertomh@users.noreply.github.com>
2026-02-26 07:01:29 +0000

    feat: implement dual-mode logging (JSON on non-TTY, Console on TTY) (#190)

--- template/{{project_name}}/log_config.py
+++ template/{{project_name}}/log_config.py
@@ -1,17 +1,24 @@
 import json
 import logging
+import sys
 
 import structlog
 
 
 def configure_logging():
+    renderer = (
+        structlog.processors.JSONRenderer(
+            serializer=lambda obj, **kwargs: json.dumps(obj, ensure_ascii=False)
+        )
+        if not sys.stderr.isatty()
+        else structlog.dev.ConsoleRenderer()
+    )
+
     structlog.configure(
         processors=[
             structlog.stdlib.add_log_level,
             structlog.processors.TimeStamper(fmt="iso"),
-            structlog.processors.JSONRenderer(
-                serializer=lambda obj, **kwargs: json.dumps(obj, ensure_ascii=False)
-            ),
+            renderer,
         ],
         wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),
         cache_logger_on_first_use=True,
